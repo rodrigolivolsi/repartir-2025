74e4f50d7e831cb478a2e9ac25eef713bdb1b471
Alejandro Giorgi <alejandro.giorgi@grupoesfera.com.ar>
2024-08-28 17:57:19 -0300

    Fix: No se estaba configurando correctamente el test. Igualmente este test no prueba nada.

--- src/main/frontend/src/app/components/gasto-nuevo/gasto-nuevo.component.spec.ts
+++ src/main/frontend/src/app/components/gasto-nuevo/gasto-nuevo.component.spec.ts
@@ -1,14 +1,21 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { GastoNuevoComponent } from './gasto-nuevo.component';
+import { HttpClientTestingModule, } from '@angular/common/http/testing';
+import { MessageService } from 'primeng/api';
+import { IdentificarGrupoPipe } from 'src/app/pipes/identificar-grupo.pipe';
 
 describe('GastoNuevoComponent', () => {
   let component: GastoNuevoComponent;
   let fixture: ComponentFixture<GastoNuevoComponent>;
 
   beforeEach(async () => {
+    const messageServiceSpy = jasmine.createSpyObj('MessageService', ['add']);
+
     await TestBed.configureTestingModule({
-      declarations: [ GastoNuevoComponent ]
+      declarations: [ GastoNuevoComponent, IdentificarGrupoPipe, ],
+      imports: [ HttpClientTestingModule ],
+      providers: [ {provide: MessageService, useValue: messageServiceSpy}, IdentificarGrupoPipe ]
     })
     .compileComponents();
   });

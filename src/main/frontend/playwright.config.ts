cd195baa86d9c715520b42502058a90527bea9bd
Natalia Lehmann <natalia.lehmann@grupoesfera.com.ar>
2024-10-14 11:02:10 -0300

    probando con monocart-report

--- src/main/frontend/playwright.config.ts
+++ src/main/frontend/playwright.config.ts
@@ -10,7 +10,15 @@ const testDir = defineBddConfig({
 
 export default defineConfig({
   testDir,
-  reporter: 'html',
+  reporter: [['list'],
+  ['monocart-reporter', {  
+      name: "Repartir Acceptance Test Report",
+      outputFile: './monocart-report/index.html',
+      coverage: {
+        entryFilter: (entry: any) => true,
+        sourceFilter: (sourcePath: any) => sourcePath.search(/src\/.+/) !== -1,
+      }
+  }]],
   use: {
     screenshot: 'only-on-failure',
     trace: 'retain-on-failure'

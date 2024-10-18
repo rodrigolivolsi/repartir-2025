c2c57bb098cba96d44f7112f0deff9bcdc0db94c
Alejandro Giorgi <alejandro.giorgi@grupoesfera.com.ar>
2024-10-18 17:17:42 -0300

    Agrego un el driver de Bienvenida y una implementacion en Playwright

--- src/jsAcceptanceTest/steps/bienvenida-steps.js
+++ src/jsAcceptanceTest/steps/bienvenida-steps.js
@@ -4,24 +4,19 @@ const { createBdd } = require('../../main/frontend/node_modules/playwright-bdd')
 const { test } = require('../../main/frontend/fixtures');
 const { Given, When, Then } = createBdd(test);
 
-Given('el usuario accede a la aplicación', async ({ page }) => {
-    await page.goto('/');
+Given('el usuario accede a la aplicación', async ({ bienvenida }) => {
+    await bienvenida.acceder();
 });
 
-Then('se muestra el mensaje de bienvenida', async ({ page }) => {
-    let dialog = page.locator('p-dialog:has-text("Repartir")');
-    await dialog.waitFor({ state: 'hidden', timeout: 2000 });
+Then('se muestra el mensaje de bienvenida', async ({ bienvenida }) => {
+    await bienvenida.validarMensajeDeBienvenida();
 });
 
-When('decidió iniciar', async ({ page }) => {
-    await page.getByRole('textbox').fill('julian');
-    await page.locator('#iniciarBienvenidaButton').click()
+When('decidió iniciar', async ({ bienvenida }) => {
+    await bienvenida.iniciar();
 })
 
-Then('puede empezar a usarla', async ({ page }) => {
-    await page.locator("#crearGruposButton").click();
-    let nuevoGrupoDialog = page.locator('#nuevoGrupoDialog');
-        // Esperar hasta que el texto específico esté presente en el elemento del diálogo
-    await expect(nuevoGrupoDialog).toContainText('Nuevo Grupo');
+Then('puede empezar a usarla', async ({ bienvenida }) => {
+    await bienvenida.validarQueSePuedeUsar();
 });
 
--- src/main/frontend/fixtures.ts
+++ src/main/frontend/fixtures.ts
@@ -1,9 +1,9 @@
 import { test as base } from 'playwright-bdd';
 import MCR from 'monocart-coverage-reports';
 import coverageOptions from './mcr.config';
+import { BienvenidaDriver, BienvenidaPlaywright } from 'test-drivers/bienvenida-driver';
 
-
-export const test = base.extend<{ autoTestFixture: void }>({
+export const test = base.extend<{ autoTestFixture: void, bienvenida: BienvenidaDriver }>({
   autoTestFixture: [async ({ page }, use) => {
     
     const isChromium = test.info().project.name === 'chromium';
@@ -36,4 +36,7 @@ export const test = base.extend<{ autoTestFixture: void }>({
 
 
   }, { auto: true }],
+  bienvenida: async({page}, use) => {
+    use(new BienvenidaPlaywright(page))
+  },
 });

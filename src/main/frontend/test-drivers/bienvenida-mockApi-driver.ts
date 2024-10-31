38b1c2a7df7da9d7985d208ca989169e73c19d1a
Natalia Lehmann <natalia.lehmann@grupoesfera.com.ar>
2024-10-31 15:46:08 -0300

    se setea variable de ambiente para ejecutar tests rapidos

--- src/main/frontend/test-drivers/bienvenida-mockApi-driver.ts
+++ src/main/frontend/test-drivers/bienvenida-mockApi-driver.ts
@@ -12,6 +12,7 @@ export class BienvenidaMockApi extends BienvenidaE2E {
 
 
     async iniciar(): Promise<void> {
+
         await this.page.route('**/api/usuarios/**', route => route.fulfill({
             status: 200,
             contentType: "application/json"
@@ -24,6 +25,5 @@ export class BienvenidaMockApi extends BienvenidaE2E {
         }));
 
         await super.iniciar();
-
     }
 }

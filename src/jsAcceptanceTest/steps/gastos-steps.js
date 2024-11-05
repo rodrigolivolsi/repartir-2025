9893e40f295a3cea04413abce1af632256488812
Guillermo Giuliana <guillermo.giuliana@grupoesfera.com.ar>
2024-11-05 09:05:40 -0300

    agrega archivo de tests y step

---
+++ src/jsAcceptanceTest/steps/gastos-steps.js
@@ -0,0 +1,54 @@
+const { expect } = require('../../main/frontend/node_modules/@playwright/test');
+const { createBdd } = require('../../main/frontend/node_modules/playwright-bdd');
+
+const { test } = require('../../main/frontend/fixtures');
+const { Given, When, Then } = createBdd(test);
+
+
+Given('existe el grupo #{int} {string} sin gastos', async({ page },posicion, nombre) => {
+    //baseDeDatos.existeUnUnicoGrupo();//TODO ver como
+})
+
+When('el usuario selecciona agregar gasto al grupo #{int}', async ({ page }, posicion) => {
+    const agregarGastoButton = await page.waitForSelector(`#agregarGastoGruposButton-${posicion}`, { timeout: 2000 });
+    
+    await agregarGastoButton.click();
+});
+
+When('completa con el monto de $ {string}', async ({ page }, monto) => {
+    const montoInput = await page.waitForSelector('#montoGastoNuevoInput', { timeout: 2000 });
+
+    await montoInput.fill(''); 
+    await montoInput.fill(monto);
+});
+
+When('guarda el gasto', async ({ page }) => {
+    const agregarGastoButton = await page.waitForSelector('#guardarGastoNuevoButton', { timeout: 2000 });
+
+    await agregarGastoButton.click();
+});
+
+Then('ve la confirmación {string}', async ({ page }, mensaje) => {
+    const mensajesToast = await page.waitForSelector('#mensajesToast', { timeout: 2000 });
+    
+    const textoToast = await mensajesToast.innerText();
+    
+    expect(textoToast).toContain('Éxito');
+    expect(textoToast).toContain(mensaje);
+});
+
+Then('ve el total del grupo #{int} actualizado a {string}', async ({ page }, posicion, monto) => {
+    await page.waitForSelector('app-grupos table tr');
+
+    const grupoTR = await page.$$('app-grupos table tr');
+
+    expect(grupoTR).toHaveLengthGreaterThan(posicion);
+
+    const campoTDs = await grupoTR[posicion].$$('td');
+
+    const primerCampoTexto = await campoTDs[0].innerText();
+    expect(primerCampoTexto).not.toBe('');
+
+    const tercerCampoTexto = await campoTDs[2].innerText();
+    expect(tercerCampoTexto).toBe(monto);
+});

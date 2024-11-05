f780263e8312d6f046fe8de76b6efe49e79d1587
Rodrigo Bestard Pino <pacobpino@gmail.com>
2024-11-05 17:24:51 -0300

    api externa test + gitlab ci

--- src/apiExternaTest/java/ar/com/grupoesfera/repartir/RepartirApplicationTests.java
+++ src/apiExternaTest/java/ar/com/grupoesfera/repartir/RepartirApplicationTests.java
@@ -5,7 +5,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-@ActiveProfiles("integrationTestWithPersonas")
+@ActiveProfiles("integrationTestWithApiExterna")
 class RepartirApplicationTests {
 
 	@Test

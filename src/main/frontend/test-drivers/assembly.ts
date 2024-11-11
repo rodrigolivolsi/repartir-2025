6e88adf53476252c736326e3ed0c798305228606
Natalia Lehmann <natalia.lehmann@grupoesfera.com.ar>
2024-11-11 10:50:21 -0300

    se elimina expansion de array de argumentos

--- src/main/frontend/test-drivers/assembly.ts
+++ src/main/frontend/test-drivers/assembly.ts
@@ -20,20 +20,17 @@ export function wrapDriver(driver: any, adapters: any[]) {
         get(target: any, methodName: string, receiver: any) {
             return async function (...args: any[]) {
 
-                // es posible que haya que agregar más argumentos a esta lista si los steps reciben más de 6 parametros
-                const [arg0, arg1, arg2, arg3, arg4, arg5, ...rest] = args;
-
                 // cada vez que se invoca un metodo sobre el driver, recorre la lista de adapters y si ese adapter tiene 
                 // una implementación para ese método, la invoca, pasandole los argumentos expandidos
                 for(let i = 0; i < adapters?.length; i++) {
                     let adapter = adapters[i];
                     if(typeof adapter[methodName] === 'function') {
-                        await adapter[methodName](arg0, arg1, arg2, arg3, arg4, arg5, rest);
+                        await adapter[methodName](...args);
                     } 
                 }
 
                 // por ultimo invoca el mismo metodo sobre el driver
-                await driver[methodName](arg0, arg1, arg2, arg3, rest);
+                await driver[methodName](...args);
             };
         }
     };

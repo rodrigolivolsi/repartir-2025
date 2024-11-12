3f00e91fedc7acbbc54f94eb35aca6543469ea5e
Guillermo Giuliana <guillermo.giuliana@grupoesfera.com.ar>
2024-11-12 11:09:28 -0300

    actualiza nombre de metodo test aceptacion java

--- src/acceptanceTest/java/ar/com/grupoesfera/repartir/steps/gastos/AgregarGastosJourneySteps.java
+++ src/acceptanceTest/java/ar/com/grupoesfera/repartir/steps/gastos/AgregarGastosJourneySteps.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import static org.openqa.selenium.support.ui.ExpectedConditions.visibilityOfElementLocated;
 
 public class AgregarGastosJourneySteps extends CucumberSteps {
-    @Step("el usuario agrega un gasto ${string} de al grupo #{string}")
+    @Step("el usuario agrega un gasto de ${string} al grupo #{string}")
     public void elUsuarioIniciaLaAplicacion(String gasto,String grupoId) {
         var wait = new WebDriverWait(driver, 2);
         var agregarGastoButton = wait.until(visibilityOfElementLocated(By.id("agregarGastoGruposButton-" + grupoId)));
--- src/acceptanceTest/resources/features/gastos/agregar_gasto_journey.feature
+++ src/acceptanceTest/resources/features/gastos/agregar_gasto_journey.feature
@@ -6,5 +6,5 @@ Característica: Experiencia al agregar gastos a los grupos
   Escenario: Creo un grupo con integrantes y agrego un gasto
   * el usuario inicia la aplicación
   * el usuario crea un grupo indicando el nombre 'Viaje a Rosario'
-  * el usuario agrega un gasto $'5000' de al grupo #'1'
+  * el usuario agrega un gasto de $'5000' al grupo #'1'
   * se muestra 1° el grupo 'Viaje a Rosario' con total '$  5.000,00'

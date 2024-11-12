9840849fab5f0fd2df2ab6eea7992768f216e9dc
Guillermo Giuliana <guillermo.giuliana@grupoesfera.com.ar>
2024-11-12 15:02:07 -0300

    actualiza metodo en gastos para evitar problemas de visibilidad durante ejecuccion de pipeline

--- src/jsAcceptanceTest/steps/gastos-steps.js
+++ src/jsAcceptanceTest/steps/gastos-steps.js
@@ -3,20 +3,23 @@ const { createBdd } = require('../../main/frontend/node_modules/playwright-bdd')
 const { test } = require('../../main/frontend/fixtures');
 const { When } = createBdd(test);
 
-When('el usuario selecciona el grupo creado y agrega un monto de ${string}', async ({ page },monto) => {
+When('el usuario selecciona el grupo creado y agrega un monto de ${string}', async ({ page }, monto) => {
     const ultimaFila = page.locator('app-grupos table tr').last();
 
     const grupoId = await ultimaFila.locator('td:nth-child(1)').textContent();
-    const agregarGastoButton = await page.waitForSelector(`#agregarGastoGruposButton-${grupoId}`, { timeout: 2000 });
+    const agregarGastoButton = page.locator(`#agregarGastoGruposButton-${grupoId}`);
     
+    await agregarGastoButton.waitFor({ state: 'visible', timeout: 2000 });
     await agregarGastoButton.click();
 
-    const montoInput = await page.waitForSelector('#montoGastoNuevoInput', { timeout: 2000 });
-
+    const montoInput = page.locator('#montoGastoNuevoInput');
+    
+    await montoInput.waitFor({ state: 'visible', timeout: 2000 });
     await montoInput.fill(''); 
     await montoInput.fill(monto);
 
-    const guardarGastoButton = await page.waitForSelector('#guardarGastoNuevoButton', { timeout: 2000 });
-
+    const guardarGastoButton = page.locator('#guardarGastoNuevoButton');
+    
+    await guardarGastoButton.waitFor({ state: 'visible', timeout: 2000 });
     await guardarGastoButton.click();
 });

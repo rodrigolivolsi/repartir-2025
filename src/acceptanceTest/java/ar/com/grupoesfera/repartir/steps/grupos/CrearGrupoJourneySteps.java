06881424697dc9192895fce09f1984730eea870b
Guillermo Giuliana <guillermo.giuliana@grupoesfera.com.ar>
2024-11-12 09:21:46 -0300

    mejora metodo en tests integracion java

--- src/acceptanceTest/java/ar/com/grupoesfera/repartir/steps/grupos/CrearGrupoJourneySteps.java
+++ src/acceptanceTest/java/ar/com/grupoesfera/repartir/steps/grupos/CrearGrupoJourneySteps.java
@@ -3,7 +3,10 @@ package ar.com.grupoesfera.repartir.steps.grupos;
 import ar.com.grupoesfera.repartir.steps.CucumberSteps;
 import ar.com.grupoesfera.repartir.steps.Step;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import java.util.List;
 
 import static org.openqa.selenium.support.ui.ExpectedConditions.*;
 import static org.assertj.core.api.Assertions.*;
@@ -20,16 +23,21 @@ public class CrearGrupoJourneySteps extends CucumberSteps {
 
     @Step("se muestra {int}° el grupo {string} con total {string}")
     public void seMuestraElNuevoGrupo(int posicion, String nombre, String total) {
-
         var wait = new WebDriverWait(driver, 2);
-        wait.until(visibilityOfElementLocated(By.id("mensajesToast")));
 
-        var grupoTR = driver.findElements(By.cssSelector("app-grupos table tr"));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("mensajesToast")));
+
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("app-grupos table")));
+
+        List<WebElement> grupoTR = wait.until(ExpectedConditions.numberOfElementsToBeMoreThan(By.cssSelector("app-grupos table tr"), posicion));
         assertThat(grupoTR).hasSizeGreaterThan(posicion);
 
-        var campoTDs = grupoTR.get(posicion).findElements(By.tagName("td"));
+        WebElement fila = grupoTR.get(posicion);
+        List<WebElement> campoTDs = wait.until(ExpectedConditions.visibilityOfAllElements(fila.findElements(By.tagName("td"))));
+
         assertThat(campoTDs.get(0).getText()).isNotEmpty();
         assertThat(campoTDs.get(1).getText()).isEqualTo(nombre);
         assertThat(campoTDs.get(2).getText()).isEqualTo(total);
     }
+
 }

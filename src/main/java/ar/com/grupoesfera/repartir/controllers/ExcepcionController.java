8ca720466ea0883b1846de33d21686b288da7873
Guillermo Giuliana <guillermo.giuliana@grupoesfera.com.ar>
2025-01-08 18:28:54 -0300

    agrega excepcion grupoNoEncontrado

--- src/main/java/ar/com/grupoesfera/repartir/controllers/ExcepcionController.java
+++ src/main/java/ar/com/grupoesfera/repartir/controllers/ExcepcionController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 
 import ar.com.grupoesfera.repartir.exceptions.GrupoInvalidoException;
+import ar.com.grupoesfera.repartir.exceptions.GrupoNoEncontradoException;
 
 @ControllerAdvice
 public class ExcepcionController {
@@ -13,6 +14,11 @@ public class ExcepcionController {
     @ExceptionHandler(GrupoInvalidoException.class)
     public ResponseEntity<String> handleGrupoInvalidoException(GrupoInvalidoException ex, WebRequest request) {
         return ResponseEntity.badRequest().body(ex.getMessage());
+    }
+
+    @ExceptionHandler(GrupoNoEncontradoException.class)
+    public ResponseEntity<String> handleGrupoNoEncontradoException(GrupoNoEncontradoException ex, WebRequest request) {
+        return ResponseEntity.notFound().build();
     }
 
     @ExceptionHandler(Exception.class)
